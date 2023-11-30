31b46de7702ed698d151af14cfb5b873aad0688b
hamza <107852931+dimahmz@users.noreply.github.com>
2023-11-30 02:24:08 +0100

    fix: cors allow all

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://restaurant-web-app-mu.vercel.app'],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
